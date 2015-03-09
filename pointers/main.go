fb14eb1b31cfa14d6e778f5dbc9ec3bc15d22c93
Nuatu <nuatut@gmail.com>
2015-03-08 21:56:33 -0700

    Add swap solution for pointers  exercise challenge

--- pointers/main.go
+++ pointers/main.go
@@ -4,12 +4,22 @@ import "fmt"
 
 func main() {
 	num := 5
+	x, y := 20, 40
+
+	swap(&x, &y)
+	fmt.Println(x, y)
 
 	double(num)
 	fmt.Println(num)
 
 	triple(&num)
 	fmt.Println(num)
+}
+
+func swap(x, y *int) {
+	temp_x := *x
+	*x = *y
+	*y = temp_x
 }
 
 func double(n int) {
