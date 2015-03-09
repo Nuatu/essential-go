aa866ca3b43ccc68cb197ba81dddd3fc9de65216
Nuatu <nuatut@gmail.com>
2015-03-09 00:05:36 -0700

    Add slice example with for loop iterating through range of slice

--- arrays-and-slices/main.go
+++ arrays-and-slices/main.go
@@ -4,9 +4,23 @@ import "fmt"
 
 func main() {
 	var nums [5]int
-	fmt.Println("empty:", nums)
+	fmt.Println("new empty array:", nums)
 
 	nums[4] = 100
 	fmt.Println("set:", nums)
 	fmt.Println("get:", nums[4])
+
+	ints := []int{1, 2, 3, 4, 5}
+	fmt.Println("new slice:", ints)
+
+	ints = append(ints, 6)
+	fmt.Println("appended slice:", ints)
+
+	fmt.Println("0-2:", ints[:2])
+	fmt.Println("2-4:", ints[2:4])
+	fmt.Println("4-6:", ints[4:])
+
+	for key, val := range ints {
+		fmt.Println(key, val)
+	}
 }
