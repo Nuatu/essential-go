acc6167b82e1fd54be060c22529b3aa551a2b011
Nuatu <nuatut@gmail.com>
2015-03-14 13:17:20 -0700

    Add more types and functions to support and take animal interface as input

--- interfaces/main.go
+++ interfaces/main.go
@@ -10,12 +10,20 @@ type Dog struct {
 	name string
 }
 
+type Fish struct {
+	name string
+}
+
 func (c Cat) Name() string {
 	return c.name
 }
 
 func (d Dog) Name() string {
 	return d.name
+}
+
+func (f Fish) Name() string {
+	return f.name
 }
 
 func (c Cat) Pet() {
@@ -26,9 +34,26 @@ func (d Dog) Pet() {
 	fmt.Println(" 'bigg dogg entertainment' ")
 }
 
+func (f Fish) Pet() {
+	fmt.Println(" 'you trippin, ain't nobody got time for that' ")
+}
+
+func (c Cat) Eat() {
+	fmt.Println("   'love me some catnip'   ")
+}
+
+func (c Dog) Eat() {
+	fmt.Println("   'throw a dogg a bone'   ")
+}
+
+func (c Fish) Eat() {
+	fmt.Println("   'who got that seaweed yo'   ")
+}
+
 type Animal interface {
-	Pet()
 	Name() string
+	Pet()
+	Eat()
 }
 
 func Compliment(a Animal) {
@@ -37,13 +62,19 @@ func Compliment(a Animal) {
 }
 
 func Feed(a Animal) {
-	fmt.Println("Eat it all:", a.Name)
+	a.Eat()
 }
 
 func main() {
 	c := Cat{"ally cat"}
 	Compliment(c)
+	Feed(c)
 
 	d := Dog{"snoop dogg"}
 	Compliment(d)
+	Feed(d)
+
+	f := Fish{"slick rick"}
+	Compliment(f)
+	Feed(f)
 }
